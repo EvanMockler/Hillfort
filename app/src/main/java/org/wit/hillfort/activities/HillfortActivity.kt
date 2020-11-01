e79936dd4780358bd3e67972449d879e220a0fd9
EvanMockler <evanmockler@gmail.com>
2020-11-01 12:45:20 +0000

    Cancel hillfort function added

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -1,6 +1,8 @@
 package org.wit.hillfort.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
@@ -17,10 +19,12 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        info("Hillfort Activity started..")
         setContentView(R.layout.activity_hillfort)
-        app = application as MainApp
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
+        info("Hillfort Activity started..")
 
+        app = application as MainApp
 
         btnAdd.setOnClickListener() {
             hillfort.title = hillfortTitle.text.toString()
@@ -37,5 +41,18 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
                 toast("Please Enter a title")
             }
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_hillfort, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

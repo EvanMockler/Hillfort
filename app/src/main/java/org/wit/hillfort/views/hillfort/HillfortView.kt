f37a7c9dfc54bfe18fa8776d812253501c6d607e
EvanMockler <evanmockler@gmail.com>
2021-01-09 15:57:22 +0000

    FireBase Storage added

--- app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
+++ app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.bumptech.glide.Glide
 import com.google.android.gms.maps.GoogleMap
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
@@ -49,7 +50,7 @@ class HillfortView : BaseView(), AnkoLogger {
     override fun showHillfort(hillfort: HillfortModel) {
         if (hillfortTitle.text.isEmpty()) hillfortTitle.setText(hillfort.title)
         if (description.text.isEmpty())  description.setText(hillfort.description)
-        hillfortImage.setImageBitmap(readImageFromPath(this, hillfort.image))
+        Glide.with(this).load(hillfort.image).into(hillfortImage);
 
         if (hillfort.image != null) {
             chooseImage.setText(R.string.change_hillfort_image)
